76ba1a155832cd30f36781bf32891a464d1eb727
ture1996 <turanjanin1996@gmail.com>
2022-11-25 01:20:19 +0100

    Second commit

---
+++ database/migrations/2022_11_24_225741_create_table_movies.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('director');
+            $table->integer('release date');
+            $table->longText('storyline');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_movies');
+    }
+};
